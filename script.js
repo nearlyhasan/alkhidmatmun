00c53cd3e6fb1cc6a10a6805c415b9f8e8b0909c
Hasan <116442148+nearlyhasan@users.noreply.github.com>
2025-07-27 03:54:49 +0500

    optimised on ios and bugs removed

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let menuOpen = false;
+  document.querySelectorAll('.nav-list a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (!target) return;
 
-function toggleMenu() {
-  const menu = document.querySelector('.menuWrap');
-  menu.classList.toggle('open');
-  menuOpen = !menuOpen;
-}
+      const rect = target.getBoundingClientRect();
+      const offset = rect.top + window.scrollY;
+      const centerScroll = offset - (window.innerHeight / 2) + (rect.height / 2);
+
+      window.scrollTo({
+        top: centerScroll,
+        behavior: 'smooth'
+      });
+    });
+  });
