44294edb4919e2f5d84fdb51005954281c688eb9
Hasan <116442148+nearlyhasan@users.noreply.github.com>
2025-07-27 04:18:06 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-  document.querySelectorAll('.nav-list a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      if (!target) return;
+document.querySelectorAll('.nav-list a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (!target) return;
 
-      const rect = target.getBoundingClientRect();
-      const offset = rect.top + window.scrollY;
-      const centerScroll = offset - (window.innerHeight / 2) + (rect.height / 2);
+    const rect = target.getBoundingClientRect();
+    const offset = rect.top + window.scrollY;
+    const centerScroll = offset - window.innerHeight / 2 + rect.height / 2;
 
-      window.scrollTo({
-        top: centerScroll,
-        behavior: 'smooth'
-      });
+    window.scrollTo({
+      top: centerScroll,
+      behavior: "smooth",
     });
   });
+});
+
+document.querySelector(".navLogo").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
